52133faa17e96368337726b7cbb7e58b9272a5d7
dev <dev@aspire>
2016-12-12 17:00:02 -0600

    version 1.2.3

--- src/Validator/UserAddress.php
+++ src/Validator/UserAddress.php
@@ -19,31 +19,42 @@ class UserAddress {
     
     protected $data;
     
+    protected $editable;
+    
     public function __construct($data,$configs) {
          $this->config = $configs['checkout.delivery.mode'];
+         $this->editable = $configs['checkout.delivery.userEditable'];
          $this->data = $data;
     }    
     
     public function isValid()
     {
        $isValid = false;
-        if($this->config==='user-address'){
-            $this->isExist();
+        if($this->config==='user-address'&&$this->editable==='editable'){
+            $this->isExistAddress();
             $isValid = true;
         }
         
-        if($this->config==='none'||$this->config==='cedis'){
+        if($this->config==='cedis'&&$this->editable==='editable'){
             $isValid = true;
         }
         
         return $isValid;
     }
     
-    private function isExist()
+    private function isExistAddress()
     {
         if(!isset($this->data->userAddress)){
                throw new \InvalidArgumentException(
                     'Address_Is_Required'); 
         }
+    }
+    
+    private function isExistCedis()
+    {
+        if(!isset($this->data->cedis)){
+               throw new \InvalidArgumentException(
+                    'Cedis_Is_Required'); 
+        }        
     }
 }
