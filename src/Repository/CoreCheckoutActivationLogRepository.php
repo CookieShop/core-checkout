89c28a694f157cedfc4243b61e5aa36ed1400ee7
Mark Alfaro <mark.alfaro@adventa.mx>
2017-02-14 12:56:20 -0600

    Elimina código innecesario para el status del checkout

--- src/Repository/CoreCheckoutActivationLogRepository.php
+++ src/Repository/CoreCheckoutActivationLogRepository.php
@@ -15,10 +15,10 @@ use Adteam\Core\Checkout\Entity\CoreCheckoutActivationLog;
 
 class CoreCheckoutActivationLogRepository extends EntityRepository
 {
-   
+
     /**
      * Fetch all config values
-     * 
+     *
      * @return array
      */
     public function fetchLast()
@@ -31,24 +31,17 @@ class CoreCheckoutActivationLogRepository extends EntityRepository
 
         $checkoutIsActive = $rangeStart <= $currentTime && $currentTime <= $rangeEnd;
 
-        $result = $this->createQueryBuilder('B')
-            ->select("B.status as enabled,DATE_FORMAT(B.createdAt,'%d-%m-%Y" .
-                " %H:%i:%s') as version")
-            ->innerJoin('B.requestedBy', 'R')
-            ->orderBy('B.createdAt', 'DESC')
-            ->getQuery()->getResult();
-        if (isset($result[0])) {
-            $checkout = (array)$result[0];
-            $checkout['enabled'] = $checkoutIsActive;
-            return $checkout;
-        }
-        throw new \InvalidArgumentException(
-            'Inicializar_canje');
+        $checkout['version'] = '1';
+        $checkout['enabled'] = $checkoutIsActive;
+        $checkout['dateStart'] = $rangeStart;
+        $checkout['dateEnd'] = $rangeEnd;
+
+        return $checkout;
     }
-    
+
     /**
-     * verifica si esta vacio log 
-     * 
+     * verifica si esta vacio log
+     *
      * @return boolean
      */
     private function hasInit()
@@ -57,16 +50,16 @@ class CoreCheckoutActivationLogRepository extends EntityRepository
         $result = $this->createQueryBuilder('B')
            ->select("B.id, B.createdAt, R.id as userId, R.displayName, B.status")
            ->innerJoin('B.requestedBy', 'R')
-           ->getQuery()->getResult();  
+           ->getQuery()->getResult();
         if(count($result)<=0){
             $isInit = true;
         }
         return $isInit;
-        
+
     }
 
     /**
-     * 
+     *
      * @param type $data
      * @param type $identity
      * @return boolean
@@ -79,7 +72,7 @@ class CoreCheckoutActivationLogRepository extends EntityRepository
         if($this->hasInit()){
             if($enabled===TRUE){
                 $this->insertLog($user, true);
-                return true; 
+                return true;
             }else{
                 throw new \InvalidArgumentException('only_true');
             }
@@ -87,16 +80,16 @@ class CoreCheckoutActivationLogRepository extends EntityRepository
             $lastcreated = $this->fetchLast();
             if($enabled===$lastcreated['enabled']){
                $activo = $lastcreated['enabled']?'canje activo':'canje cerrado';
-               throw new \InvalidArgumentException($activo);                
+               throw new \InvalidArgumentException($activo);
             }
             $this->insertLog($user, $enabled);
-            return $enabled; 
+            return $enabled;
         }
     }
 
     /**
      * Obtiene usuario mediante username
-     * 
+     *
      * @param type $username
      * @return type
      */
@@ -109,16 +102,16 @@ class CoreCheckoutActivationLogRepository extends EntityRepository
                 ->setParameter('username', $username)
                 ->getQuery()->getSingleResult();
     }
-    
+
     /**
      * Inserta log de apertura y cierre
      * de canje
-     * 
+     *
      * @param type $user
      * @param type $status
      */
     public function insertLog($user,$status)
-            
+
     {
         $currentRepo = $this;
         $this->_em->transactional(
@@ -128,20 +121,20 @@ class CoreCheckoutActivationLogRepository extends EntityRepository
                             OauthUsers::class, $user['id']);
                 $log->setRequestedBy($UserReference);
                 $log->setStatus($status);
-                $em->persist($log);             
+                $em->persist($log);
                 $currentRepo->updateCheckoutEnabled($status);
             }
         );
     }
-    
+
     /**
      * update estatus core config
-     * 
+     *
      * @param type $status
      * @return type
      */
     private function updateCheckoutEnabled($status)
-    {    
+    {
         $s = (int)$status;
         $dql ="UPDATE Adteam\Core\Checkout\Entity\CoreConfigs U SET U.value = '"
                 . $s."' WHERE U.key like 'checkout.enabled'";
